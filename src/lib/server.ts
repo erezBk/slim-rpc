5b825f5f2fdf94c7ed43d1a95a15ea1f7d830e27
erezBk <erezbkdeveloper@gmail.com>
2024-01-13 21:45:47 +0200

    comments

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -8,7 +8,24 @@ import {
 import { SafeParseError, Schema } from "zod";
 let app: WebFramework;
 
-const arr_to_obj = (arr: string[]) => {
+/**
+ * function to turn all of the rpc api routes into an object
+ * that is nested the same way as the paths
+ * for example :
+ * given arr = ['users/list','users/add','accounts/list','accounts/remove']
+ * the resulting object will be
+ * {
+ *   "users": {
+ *       "list": "users/list",
+ *       "add": "users/add"
+ *   },
+ *   "accounts": {
+ *       "list": "accounts/list",
+ *       "remove": "accounts/remove"
+ *   }
+ * }
+ * */
+const to_object_paths = (arr: string[]) => {
   let result: { [key: string]: any } = {};
   arr.forEach((item) => {
     const keys = item.split("/");
@@ -34,8 +51,8 @@ export const create_rpc_server = (params: {
   app.inject_ctx_to_each_call("req_context", create_context);
 
   const all_routes: string[] = [];
-  // @ts-ignore
-  function exec_path(key, value) {
+
+  function exec_path(key: string, value: any) {
     if (typeof value == "object") {
       Object.entries(value).forEach(([k, v]) => {
         exec_path(`${key}/${k}`, v);
@@ -45,12 +62,19 @@ export const create_rpc_server = (params: {
       value(key);
     }
   }
-  // @ts-ignore
+
   Object.entries(routes).forEach(([k, v]) => exec_path(k, v));
   // route for client to get the slim-rpc scheme
-  app.expose_all_routes("/slim-rpc-scheme", arr_to_obj(all_routes));
+  app.expose_all_routes("/slim-rpc-scheme", to_object_paths(all_routes));
 };
 
+/**
+ * Complicated shit ahead warning!
+ * ok, in order to create the route I needed to return
+ * a function which lets the web framework create the route in a later stage
+ * because at the time of the RPC function is being used in the modules (ex : users , account ,cart ..)
+ * it has no idea about the routes structure which is being constructed only when calling the create_rpc_server() function
+ * */
 export const RPC = <IN, OUT>(
   zod_scheme: Schema,
   fn: (input: IN, ctx: RpcRequestContext) => Promise<OUT>
@@ -64,12 +88,14 @@ export const RPC = <IN, OUT>(
           const value = await fn(input, {
             ctx,
           });
+          // all good
           return {
             type: "success",
             value,
           };
         } catch (error) {
           const { message } = error as Error;
+          // some internal error
           return {
             type: "error",
             code: 500,
@@ -77,6 +103,7 @@ export const RPC = <IN, OUT>(
           };
         }
       } else {
+        // input validation error
         return {
           type: "error",
           code: 400,
@@ -87,6 +114,11 @@ export const RPC = <IN, OUT>(
       }
     });
   };
+  /**
+   * Typescript workaround alert!
+   * I wanted the client to perform for ex : client.users.list.query({count:5})  with full intellisense
+   * so I tricked typescript using this @ts-ignore below to enable the intellisense on the client
+   * */
   // @ts-ignore
   return create_route;
 };
